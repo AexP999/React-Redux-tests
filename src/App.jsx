bf8ee1a5878b56fc842204705e2f1203a38aaa25
Alex <mikol999@gmail.com>
2022-09-11 22:22:10 +0300

    en

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,9 @@ import TodoList from './components/TodoList';
 import InputFields from './components/InputFields';
 
 function App() {
-  // const [todos, setTodos] = useState([]);
-
-  // const addTodo = () => {
-  // if (text.trim().length) {
-  //   setTodos([
-  //     ...todos,
-  //     {
-  //       id: new Date().toISOString(),
-  //       text,
-  //       completed: false,
-  //     },
-  //   ]);
-  //   setText('');
-  // }
-  // };
-
-  // const deleteTodo = (todoId) => {
-  //   setTodos(todos.filter((todo) => todo.id !== todoId));
-  // };
-
-  // const toggleTodoCompleted = (todoId) => {
-  //   setTodos(
-  //     todos.map((todo) => {
-  //       if (todo.id !== todoId) return todo;
-
-  //       return { ...todo, completed: !todo.completed };
-  //     })
-  //   );
-  // };
-
   return (
     <div className='App'>
       <InputFields />
-
       <TodoList />
     </div>
   );
