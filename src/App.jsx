9949d8d86c2b1d2bb86e72e21a46f6911da0605d
Alex <mikol999@gmail.com>
2022-09-22 20:52:19 +0300

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,56 @@
 import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
 import './App.css';
-import { useState } from 'react';
-import TodoList from './components/TodoList';
-import InputFields from './components/InputFields';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [text, setText] = useState('');
-
-  const addTodo = () => {
-    if (text.trim().length) {
-      setTodos([
-        ...todos,
-        {
-          id: new Date().toISOString(),
-          text,
-          completed: false,
-        },
-      ]);
-      setText('');
-    }
-  };
-
-  const deleteTodo = (todoId) => {
-    setTodos(todos.filter((todo) => todo.id !== todoId));
-  };
-
-  const toggleTodoCompleted = (todoId) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id !== todoId) return todo;
-
-        return { ...todo, completed: !todo.completed };
-      })
-    );
-  };
-
   return (
     <div className='App'>
-      <InputFields addTodo={addTodo} text={text} setText={setText} />
-
-      <TodoList
-        todos={todos}
-        toggleTodoCompleted={toggleTodoCompleted}
-        deleteTodo={deleteTodo}
-      />
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className='App-link'
+            href='https://reactjs.org/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className='App-link'
+            href='https://redux.js.org/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className='App-link'
+            href='https://redux-toolkit.js.org/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className='App-link'
+            href='https://react-redux.js.org/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
     </div>
   );
 }
